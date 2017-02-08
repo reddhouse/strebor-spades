ed0e66753c48b1bc22ea7a3e640bcff4a8ac2a6c
Justin Tuttle <reddhouse@gmail.com>
2017-02-08 11:21:38 -0800

    Adjust style on Home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -31,7 +31,7 @@
     <div class="flex-container">
       <div class="flex1"></div>
       <div class="flex2">{{ playerNames[0].name }}</div>
-      <div class="flex3"><router-link to="/player1">Mobile</router-link>&emsp;</div>
+      <div class="flex3"><router-link to="/player1">Mobile</router-link></div>
       <div class="flex4">&emsp;<router-link to="/player1-solo">Solo</router-link></div>
       <div class="flex5">
         <input type="text" v-model="localPlayer1" placeholder="Edit Name">
@@ -44,7 +44,7 @@
     <div class="flex-container">
       <div class="flex1"></div>
       <div class="flex2">{{ playerNames[2].name }}</div>
-      <div class="flex3"><router-link to="/player3">Mobile</router-link>&emsp;</div>
+      <div class="flex3"><router-link to="/player3">Mobile</router-link></div>
       <div class="flex4">&emsp;<router-link to="/player3-solo">Solo</router-link></div>
       <div class="flex5">
         <input type="text" v-model="localPlayer3" placeholder="Edit Name">
@@ -67,7 +67,7 @@
     <div class="flex-container">
       <div class="flex1"></div>
       <div class="flex2">{{ playerNames[1].name }}</div>
-      <div class="flex3"><router-link to="/player2">Mobile</router-link>&emsp;</div>
+      <div class="flex3"><router-link to="/player2">Mobile</router-link></div>
       <div class="flex4">&emsp;<router-link to="/player2-solo">Solo</router-link></div>
       <div class="flex5">
         <input type="text" v-model="localPlayer2" placeholder="Edit Name">
@@ -80,7 +80,7 @@
     <div class="flex-container">
       <div class="flex1"></div>
       <div class="flex2">{{ playerNames[3].name }}</div>
-      <div class="flex3"><router-link to="/player4">Mobile</router-link>&emsp;</div>
+      <div class="flex3"><router-link to="/player4">Mobile</router-link></div>
       <div class="flex4">&emsp;<router-link to="/player4-solo">Solo</router-link></div>
       <div class="flex5">
         <input type="text" v-model="localPlayer4" placeholder="Edit Name">
@@ -193,7 +193,7 @@ export default {
 }
 
 .flex5 {
-  width: 170px;
+  width: 195px;
   flex: 0 1 auto;
 }
 
