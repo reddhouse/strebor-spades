949984de846e660ae80b2ae54dc33f66e1eec3ae
Justin Tuttle <reddhouse@gmail.com>
2017-02-08 10:21:58 -0800

    Add socket.io

--- server.js
+++ server.js
@@ -8,3 +8,15 @@ app.use('/api', jsonServer.router('server/db.json'));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
+
+// WS experiment
+const socketIO = require('socket.io');
+const io = socketIO(app);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+// END WS experiment
